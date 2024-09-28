8e63cd3d3f954bf430777115bdfa0d341fe77741
rolly <rollycyber@gmail.com>
2024-09-28 17:11:10 +0700

    db

--- db.js
+++ db.js
@@ -1,14 +1,20 @@
-const mysql = require('mysql');
+const mysql = require('mysql'); // Jika kamu menggunakan mysql v1
+// Jika kamu menggunakan mysql2 (versi lebih baru), gunakan:
+// const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: '202.10.41.174',
-    user: 'root',
-    password: 'ontageniT1!',
-    database: 'crud_db'
+    host: '202.10.41.174',      // IP VPS atau server MySQL
+    user: 'root',               // Username MySQL
+    password: 'ontageniT1!',     // Password MySQL
+    database: 'crud_db',         // Nama database yang digunakan
+    port: 3306                   // Tambahkan port jika menggunakan port berbeda, defaultnya 3306
 });
 
 db.connect(err => {
-    if (err) throw err;
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
     console.log('MySQL Connected');
 });
 
